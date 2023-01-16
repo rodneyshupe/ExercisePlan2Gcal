f41ceec7f9947bf3c5f78fe6e93e2b2f72135ba1
rodneyshupe <rodney@shupe.ca>
2023-01-15 21:47:43 -0800

    Imrproved calendar naming

--- test/test.js
+++ test/test.js
@@ -2,8 +2,10 @@ const test = () => {
     const now = new Date();
     const startDate = new Date(now.getFullYear(), now.getMonth(), now.getDate());
 
-    const calendarName = 'Test Calendar ' + now.getFullYear() + now.getMonth() + now.getDate();
+    const calendarName = 'Test Calendar ' + now.toISOString().substring(0, 10);
+
     const calendar = CalendarApp.createCalendar(calendarName);
+    Logger.log('Calendar Created: ' + calendarName);
     const calendarId = calendar.getId();
 
     // Call the createCalendarEvents function and pass in the JSON object, the target calendarname, and the new start date
