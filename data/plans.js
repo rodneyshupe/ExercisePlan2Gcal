043006a8a8327699155e40178a70139b06c5ee01
rodneyshupe <rodney@shupe.ca>
2023-01-15 18:49:09 -0800

    Add plans and put data in its own directory

---
+++ data/plans.js
@@ -0,0 +1,10 @@
+const plans =  [
+    {
+        "plan": "Couch to 5k (C25K)",
+        "events": events_c25k
+    },
+    {
+        "plan": "5K to 10k (5K210K)",
+        "events": events_c5k210k
+    },
+];
